ef0a210e3712ddfe200961f6aabf2ed20f6eda7d
Tom Conchie <tconchie@gmail.com>
2017-08-07 17:11:32 -0600

    Adds move history tracking list, readies for implementation of jumping to previous moves

--- src/index.js
+++ src/index.js
@@ -74,7 +74,18 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[history.length-1];
-    const winner = calculatewinner(current.squares);
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        `Move# ${move}` :
+        `Game start`;
+        return (
+          <li>
+            <a href="#" onClick={()=> this.jumpTo(move)}>{desc}</a>
+          </li>
+        );
+    });
 
     let status;
     if(winner)
@@ -92,7 +103,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
